be474f4f221b68dcd63a2c6af8912e105e0f5bab
bhagyesh508 <bhagyeshyadav29@gmail.com>
2026-01-09 17:36:33 +0530

    fteched products from database on with condition

--- testapp/views.py
+++ testapp/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from testapp.models import Task
 
 def Home(request):
-    tasks = Task.objects.filter(is_completed=False)
+    tasks = Task.objects.filter(is_completed=False).order_by('-updated_at') # order by will show data in ascending order and if passed as negative it will show in descending order
     #filter takes if condition as parameter for retrieving data from task model form db
+    completed_tasks = Task.objects.filter(is_completed = True)
     context={
-        'tasks':tasks
+        'tasks':tasks,
+        'completed_task':completed_tasks
         #sending key from here and receiving the key in html template to access the db fields via context dictionary
     }
     return render (request,'home.html',context)
@@ -15,4 +17,4 @@ def addTask(request):
     task_name = request.POST['task_name']
     task_desc = request.POST['task_desc']
     Task.objects.create(task_name=task_name,task_desc=task_desc)
-    return HttpResponse('the form is submitted')
+    return redirect ('home')
