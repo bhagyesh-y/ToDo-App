e0bb69e0f41f0e0444ed62809babbc46feb0b779
bhagyesh508 <bhagyeshyadav29@gmail.com>
2026-01-09 16:38:12 +0530

    added logic for creating tasks in db from frontend via user input

--- testapp/admin.py
+++ testapp/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Task
 
-admin.site.register(Task)
-# Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+      list_display=("task_name","is_completed","updated_at")
+      search_fields=("task_name",)
+admin.site.register(Task,TaskAdmin)
+
--- testapp/views.py
+++ testapp/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from testapp.models import Task
 
 def Home(request):
-    return render (request,'home.html')
+    tasks = Task.objects.filter(is_completed=False)
+    #filter takes if condition as parameter for retrieving data from task model form db
+    context={
+        'tasks':tasks
+        #sending key from here and receiving the key in html template to access the db fields via context dictionary
+    }
+    return render (request,'home.html',context)
+
+def addTask(request):
+    task_name = request.POST['task_name']
+    task_desc = request.POST['task_desc']
+    Task.objects.create(task_name=task_name,task_desc=task_desc)
+    return HttpResponse('the form is submitted')
